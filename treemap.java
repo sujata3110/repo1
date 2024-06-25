1bc0739300766800bcb89ad7f8e46ea18178c101
unknown <you@example.com>
2024-06-25 11:37:43 +0530

    first file done...

---
+++ treemap.java
@@ -0,0 +1,11 @@
+import java.util.*;
+ class Test{
+public static void main(String args[]){
+    TreeMap Tmap= new TreeMap();
+	Tmap.put(6,7);
+ 	 Tmap.put(3,"Hello");
+		Tmap.put(5,12);
+		Tmap.put(7,43);
+System.out.println(Tmap);
+}
+}
