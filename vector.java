1f78711fabb070e971dcde110a60fd7c4f044479
unknown <you@example.com>
2024-06-25 11:19:07 +0530

    sfdgd

---
+++ vector.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Test{
+public static void main(String args[])
+    {
+ Vector vt=new Vector();
+vt.add(10);
+vt.add(23);
+vt.add(34.7);
+vt.add(true);
+vt.add("Hello");
+System.out.println(vt);
+vt.add(65);
+vt.add(67);
+System.out.println(vt);
+   }
+}
